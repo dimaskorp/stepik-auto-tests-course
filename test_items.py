c3e63a57b1d01019280324529f68971833df1a57
dimaskorp <52814195+dimaskorp@users.noreply.github.com>
2021-09-01 14:04:47 +0400

    Update test_items.py
    Тест проверяет, что страница товара на сайте содержит кнопку добавления в корзину.
    Тест запускается с параметром language следующей командой: pytest --language=es test_items.py

--- test_items.py
+++ test_items.py
@@ -1,1 +1,8 @@
+import time
 
+def test_items(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    browser.get(link)
+    time.sleep(30)
+    button = browser.find_element_by_css_selector("button.btn")
+    assert button, "Кнопка не найдена"
