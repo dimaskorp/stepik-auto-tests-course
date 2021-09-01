46469edafc062b9bbbe918caaf31fc0b60e82915
dimaskorp <52814195+dimaskorp@users.noreply.github.com>
2021-09-01 14:03:04 +0400

    Update conftest.py
    Добавлен обработчик, который считывает из командной строки параметр language.
    Реализована логика запуска браузера с указанным языком пользователя. 
    Браузер объявляется в фикстуре browser и передаваться в тест как параметр.

--- conftest.py
+++ conftest.py
@@ -1,1 +1,24 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None, help="Выберите язык")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language_name = request.config.getoption("language")
+    print("\nstart browser for test..")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language_name})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+
+
 
