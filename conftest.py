383d77431f9ce1cdc02949dd807ebbbdf2d442e4
dimaskorp <dima-skorp@mail.ru>
2021-09-06 16:28:44 +0400

    Создан пустой файл __init__.py, чтобы работали относительные импорты

--- conftest.py
+++ conftest.py
@@ -4,16 +4,22 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None, help="Выберите язык")
+    """Parse command args for select language for Chrome browser."""
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language for browser")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    language_name = request.config.getoption("language")
-    print("\nstart browser for test..")
+    """Start Chrome browser for test-funcs. Add language options from command args.
+    End close browser after every test-func."""
+    selected_language = request.config.getoption("language")
+    browser = None
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': language_name})
+    options.add_experimental_option('prefs', {'intl.accept_languages': selected_language})
     browser = webdriver.Chrome(options=options)
+    print(f'\nStart Chrome browser with "{selected_language}" language')
+
     yield browser
     print("\nquit browser..")
     browser.quit()
--- test_items.py
+++
@@ -1,8 +0,0 @@
-import time
-
-def test_items(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-    browser.get(link)
-    time.sleep(30)
-    button = browser.find_element_by_css_selector("button.btn")
-    assert button, "Кнопка не найдена"
