a31074df65d554ea9dbe7e4be0694b49c0436739
dimaskorp <dima-skorp@mail.ru>
2021-09-10 14:49:26 +0400

    создан класс LoginPageLocators добавлена проверка, что мы действительно перешли на страницу логина

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    LOGIN_USERNAME = (By.CSS_SELECTOR, "#login-username")
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, "#registration-email")
